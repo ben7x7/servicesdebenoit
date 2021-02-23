880e9582290c88f3f9c000a3cff8a3662dc7105f
benoit bessiere <benoit.bessiere@gmail.com>
2021-02-23 17:25:37 +0100

    add mentions legales

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'cookys',           to: 'pages#cookys',             as: :cookys
+  get 'mentions',         to: 'pages#mentions',           as: :mentions
+  get 'confidentialite',  to: 'pages#confidentialite',    as: :confidentialite
+
+  #resources :services, only: [ :new, :create, :index, :show ]
 end
+
+
+
+
+
