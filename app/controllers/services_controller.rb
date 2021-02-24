fe5022bb66643f7dc570b8dfb45d64aab66fcea0
benoit bessiere <benoit.bessiere@gmail.com>
2021-02-24 08:13:08 +0100

    CRUD controller + view

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,28 @@
 class ServicesController < ApplicationController
+
+  def index
+    @services = Service.all
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    @service.save
+
+    redirect_to service_path(service)
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name, :fullname, :logo, :image, :definition, :conditions)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'cookys',           to: 'pages#cookys',             as: :cookys
   get 'mentions',         to: 'pages#mentions',           as: :mentions
-  get 'confidentialite',  to: 'pages#confidentialite',    as: :confidentialite
+  get 'confidentialites',  to: 'pages#confidentialites',    as: :confidentialites
 
   resources :services, only: [ :new, :create, :index, :show ]
 end
