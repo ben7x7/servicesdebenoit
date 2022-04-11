53f975ab7aaf1ad87bfa59d54b691530044defb6
benoit bessiere <benoit.bessiere@gmail.com>
2022-04-11 07:56:08 +0200

    modify errors parameters

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get 'mentions',           to: 'pages#mentions',             as: :mentions
   get 'confidentialites',   to: 'pages#confidentialites',     as: :confidentialites
 
-  match "/404", to: "errors#not_found", via: :all
-  match "/422", to: "errors#unacceptable", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
+  get "/404", to: "errors#not_found", via: :all
+  get "/422", to: "errors#unacceptable", via: :all
+  get "/500", to: "errors#internal_server_error", via: :all
 
   resources :services, only: [ :new, :create, :index, :show ] do
     resources :activities, only: [ :new, :create ]
